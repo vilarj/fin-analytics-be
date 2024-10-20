030d7b2dfe27a4b4ecf4e148aa5f285c69b1a23c
vilarj <lamaravillasan@outlook.com>
2024-10-20 16:52:44 -0400

    Report entity | Report controller | Report Service

---
+++ src/main/java/Services/ReportService.java
@@ -0,0 +1,41 @@
+package Services;
+
+import Entities.Report;
+import org.springframework.stereotype.Service;
+
+import java.sql.*;
+import java.time.LocalDate;
+
+@Service
+public class ReportService {
+    private static final String DB_URL = "jdbc:sqlite:./Database/mydb.db"; // TODO: Change this to the actual
+
+    public Report GetReport(long reportId) {
+        String query = "SELECT reportId, userId, details, lastGeneratedIn FROM Report WHERE reportId = ?";
+        Report report = null;
+
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setLong(1, reportId);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    long id = rs.getLong("ReportId");
+                    long userId = rs.getLong("UserId");
+                    String[] details = rs.getString("Details").split(",");
+                    LocalDate lastGeneratedIn = LocalDate.parse(rs.getString("LastGeneratedIn"));
+
+                    report = new Report(id, userId, details);
+                    report.setLastGeneratedIn(lastGeneratedIn);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error querying the database", e);
+        }
+
+        return report;
+    }
+
+}
---
+++ src/main/java/controllers/ReportsController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import Entities.Report;
+import Services.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/api/Reports")
+public class ReportsController {
+
+    @Autowired
+    private ReportService service;
+
+    @RequestMapping("/getReport/{reportId}")
+    public Report GetReport(@PathVariable long reportId){
+        return service.GetReport(reportId);
+    }
+}
