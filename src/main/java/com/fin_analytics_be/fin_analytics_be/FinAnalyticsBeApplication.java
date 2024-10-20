28a7da02e452036d95c9669057a6eda4d68f341e
vilarj <lamaravillasan@outlook.com>
2024-10-20 16:05:19 -0400

    init

---
+++ src/main/java/com/fin_analytics_be/fin_analytics_be/FinAnalyticsBeApplication.java
@@ -0,0 +1,13 @@
+package com.fin_analytics_be.fin_analytics_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinAnalyticsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinAnalyticsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fin_analytics_be/fin_analytics_be/FinAnalyticsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fin_analytics_be.fin_analytics_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinAnalyticsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
