8d1eb2a1412945aa0fa9725d1559dbf5d2ed6214
vilarj <lamaravillasan@outlook.com>
2024-11-03 16:58:36 -0500

    Removing old project

--- fin-analytics-be/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace fin_analytics_be.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
