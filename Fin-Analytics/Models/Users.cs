949f7e60f213391739f82a0e8ebb03ea2ff8ff9d
vilarj <lamaravillasan@outlook.com>
2024-11-04 12:02:43 -0500

    Users entity model | EF and SQLite dependencies

---
+++ Fin-Analytics/Models/Users.cs
@@ -0,0 +1,28 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Fin_Analytics.Models
+{
+
+    public class Users
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public required string FullName { get; set; }
+
+        public DateTime? DoB { get; set; }
+
+        public required string Phone { get; set; }
+
+        public required string Address { get; set; }
+
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        public required string Company { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+    }
+}
