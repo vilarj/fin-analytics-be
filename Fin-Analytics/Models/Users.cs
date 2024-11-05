6d7b951659bd1a19ce194a082b7837320ebd5714
vilarj <lamaravillasan@outlook.com>
2024-11-05 12:45:22 -0500

    XML on the Users model

--- Fin-Analytics/Models/Users.cs
+++ Fin-Analytics/Models/Users.cs
@@ -3,26 +3,65 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Fin_Analytics.Models
 {
-
+    /// <summary>
+    /// Represents a user of the financial analytics system.
+    /// </summary>
     public class Users
     {
+        /// <summary>
+        /// Gets or sets the unique identifier for the user.
+        /// </summary>
+        /// <remarks>
+        /// This is a system-generated ID that is used to uniquely identify each user.
+        /// </remarks>
         [Key]
         public int UserId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the full name of the user.
+        /// Required.
+        /// </summary>
+        /// <remarks>
+        /// This field stores the user's full name, which is used for display purposes.
+        /// </remarks>
         [Required]
         public required string FullName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the date of birth of the user.
+        /// Required.
+        /// </summary>
         public DateTime? DoB { get; set; }
 
+        /// <summary>
+        /// Gets or sets the phone number of the user.
+        /// Required.
+        /// </summary>
         public required string Phone { get; set; }
 
+        /// <summary>
+        /// Gets or sets the address of the user.
+        /// Required.
+        /// </summary>
         public required string Address { get; set; }
 
+        /// <summary>
+        /// Gets or sets the email address of the user.
+        /// Required.
+        /// </summary>
         [EmailAddress]
         public required string Email { get; set; }
 
+        /// <summary>
+        /// Gets or sets the company of the user.
+        /// Required.
+        /// </summary>
         public required string Company { get; set; }
 
+
+        /// <summary>
+        /// Gets or sets the date and time when the user was created.
+        /// </summary>
         public DateTime? CreatedOn { get; set; }
     }
 }
